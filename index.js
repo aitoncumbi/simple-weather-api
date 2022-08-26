8dac4482bbadf59fdc5369b67d00dfb76d0b4c4d
Aiton Cumbi <111422543+aitoncumbi@users.noreply.github.com>
2022-08-26 14:59:42 +0200

    changed test route to current weather, added all weather data method

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const API_KEY  = process.env.API_KEY;
 const cors = require("cors");
 
 
-app.get('/test', cors(), (req, res)=>{
+app.get('/current_weather', cors(), (req, res)=>{
   async function citty_grabber(){
 
 
@@ -60,6 +60,49 @@ app.get('/test', cors(), (req, res)=>{
 })
 
 
+app.get('/all_weather_data', cors(), function(req, res) {
+
+    async function citty_grabber(){
+      try {
+
+      let city = req.query.city;
+      let response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API_KEY}`);
+      let data = await response.json();
+      data = JSON.stringify(data);
+      data = JSON.parse(data);
+      return data;
+
+
+      }catch(error) {
+        console.log(error)
+      }
+    }
+
+
+    async function get_all_weather_data() {
+      try {
+
+        let headers = await citty_grabber();
+        console.log(headers)
+        let lat =  headers[0].lat;
+        let lon =  headers[0].lon;
+
+        let response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+        let data = await response.json();
+        data = JSON.stringify(data);
+        data = JSON.parse(data);
+
+        res.send(data)
+        
+      } catch (error) {
+        console.log(error)
+        res.send("city cannot be empty")
+      }
+    }
+
+
+    get_all_weather_data();
+})
 
 
 
